ca4caccff57635a2de0b20839011ed44474b8792
Tao Liu <tao.liu@ibm.com>
2025-12-30 14:42:47 -0500

    abc

--- closed/test/jdk/openj9/internal/security/TestProperties.java
+++ closed/test/jdk/openj9/internal/security/TestProperties.java
@@ -263,6 +263,8 @@ public class TestProperties {
 
             System.out.println("Provider Version: 2");
             System.out.println("Provider Version: 2");
+
+            System.out.println("Provider Version: 2");System.out.println("Provider Version: 2");
         } catch (Exception e) {
             System.out.println(e);
         }
